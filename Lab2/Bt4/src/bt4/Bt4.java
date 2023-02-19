c873b1f6a7dfe051b01f41cb74337d6c22fbfc93
thien1662003 <125809424+thien1662003@users.noreply.github.com>
2023-02-19 23:11:17 +0700

    Add files via upload

---
+++ Lab2/Bt4/src/bt4/Bt4.java
@@ -0,0 +1,39 @@
+package bt;
+
+import bt.Bt1;
+import bt.Bt1;
+import bt.Bt2;
+import bt.Bt2;
+import bt.Bt2;
+import bt.Bt3;
+import bt.Bt3;
+import bt.Bt3;
+import java.util.Scanner;
+
+public class Bt4
+{
+    public static void menu ()
+    {
+       System.out.println("b1");
+       System.out.println("b2");
+       System.out.println("b3");
+       Scanner sc = new Scanner(System.in);
+       int chon = sc.nextInt();
+       switch(chon)
+       {
+           case 1: Bt.ptb1();
+           break;
+           case 2: b2.ptb2();
+           break;
+           case 3:b3.tinhtiendien();
+           break;
+           case 4: break;
+           default:  
+               System.out.println("nhap sai ");
+       }
+    }
+     public static void main(String[] arg)
+   {
+       menu();
+   }
+}
